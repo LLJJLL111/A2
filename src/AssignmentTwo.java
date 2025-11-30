93983fd31798af33476acce26a40ef65025534c2
LLJJLL111 <3472746945@qq.com>
2025-12-01 05:13:49 +0800

    Add files via upload

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,63 +1,60 @@
 /**
- * Main class for the Theme Park Management System
- * Contains the main method and placeholder methods for different parts of the assignment
+ * AssignmentTwo class - Main class containing all demonstration methods
+ * Part 2 update: Added method placeholders for Part 3-7
  */
 public class AssignmentTwo {
-
-    /**
-     * Main method - entry point of the application
-     * @param args Command line arguments
-     */
+    
     public static void main(String[] args) {
-        // Main method will be implemented to demonstrate various parts
-        System.out.println("Theme Park Management System Started");
+        System.out.println("=== Theme Park Visitor Management System ===");
+        System.out.println("Part 2: Abstract Class and Interface Implementation");
+        
+        // Test basic functionality of Part 2
+        AssignmentTwo test = new AssignmentTwo();
+        test.testPart2();
     }
-
+    
     /**
-     * Placeholder method for Part 3 implementation
-     * Will demonstrate queue operations for ride waiting lines
+     * Test functionality of Part 2
      */
+    public void testPart2() {
+        System.out.println("\n--- Testing Part 2: Abstract Classes and Interfaces ---");
+        
+        // Create employee - corrected parameter order and added email
+        Employee operator = new Employee("John Smith", 28, "john.smith@themepark.com", "E001", "Ride Operations", 50000.0);
+        
+        // Create ride (implements RideInterface) - corrected parameter order
+        Ride rollerCoaster = new Ride("Thunder Coaster", "Roller Coaster", 24, operator);
+        
+        // Test interface methods (to be implemented in later parts)
+        System.out.println("Testing interface methods (to be implemented in later parts):");
+        rollerCoaster.addVisitorToQueue(null); // To be implemented in Part 3
+        rollerCoaster.runOneCycle(); // To be implemented in Part 5
+        
+        System.out.println("\nRide created: " + rollerCoaster);
+    }
+    
+    // Method placeholders for Part 3-7
     public void partThree() {
-        // To be implemented in Part 3
+        System.out.println("Part 3 method - To be implemented");
     }
-
-    /**
-     * Placeholder method for Part 4A implementation
-     * Will demonstrate ride history operations using LinkedList
-     */
+    
     public void partFourA() {
-        // To be implemented in Part 4A
+        System.out.println("Part 4A method - To be implemented");
     }
-
-    /**
-     * Placeholder method for Part 4B implementation
-     * Will demonstrate sorting of ride history
-     */
+    
     public void partFourB() {
-        // To be implemented in Part 4B
+        System.out.println("Part 4B method - To be implemented");
     }
-
-    /**
-     * Placeholder method for Part 5 implementation
-     * Will demonstrate running ride cycles
-     */
+    
     public void partFive() {
-        // To be implemented in Part 5
+        System.out.println("Part 5 method - To be implemented");
     }
-
-    /**
-     * Placeholder method for Part 6 implementation
-     * Will demonstrate writing ride history to file
-     */
+    
     public void partSix() {
-        // To be implemented in Part 6
+        System.out.println("Part 6 method - To be implemented");
     }
-
-    /**
-     * Placeholder method for Part 7 implementation
-     * Will demonstrate reading ride history from file
-     */
+    
     public void partSeven() {
-        // To be implemented in Part 7
+        System.out.println("Part 7 method - To be implemented");
     }
 }
